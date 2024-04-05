65e59e184f3a5beeb14b03b817ae17853447cdd4
Anna <aksmithnw@gmail.com>
2024-04-04 18:31:42 -0700

    Add basic functionality to add book to library
    Create form for book input, implement simple callback to add book
    data to library upon pressing submit. Refactor functions for
    adding/displaying books in table

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 myLibrary = [];
+populateLibrary(10);
+
+const addBookForm = document.querySelector("form");
+
+const addBookInputs = {
+    title : document.querySelector("#title"),
+    author : document.querySelector("#author"),
+    pages : document.querySelector("#pages"),
+    read : document.querySelector("#read"),
+    makeBook: () => {
+        return new Book(title.value, author.value, pages.value, read.value);
+    }
+};
+
+addBookForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addBookToLibrary(addBookInputs.makeBook());
+});
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -12,35 +30,23 @@ function Book(title, author, pages, read){
 
 function populateLibrary(count) {
     for(let i = 0; i < count; i++){
-        myLibrary.push(new Book(`title ${i}`, `author ${i}`, i*10, (i%3)==0));
+        addBookToLibrary(new Book(`title ${i}`, `author ${i}`, i*10, (i%3)==0));
     }
 }
 
-function addBookToLibrary() {
-    let title = prompt("Enter a title: ");
-    let author = prompt("Enter an author: ");
-    let pages = prompt("Enter number of pages: ");
-    let read = confirm("Have you read the book?");
-    myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    addToTable(book);
 }
 
-function displayBooks() {
+function addToTable(book) {
     let table = document.querySelector("table");
-    let headerRow = document.createElement("tr");
-    populateCell(headerRow, "Title", "th");
-    populateCell(headerRow, "Author", "th");
-    populateCell(headerRow, "Pages", "th");
-    populateCell(headerRow, "Read?", "th");
-    table.append(headerRow);
-    
-    for (const book of myLibrary){
-        let bookRow = document.createElement("tr");
-        populateCell(bookRow, book.title, "td");
-        populateCell(bookRow, book.author, "td");
-        populateCell(bookRow, book.pages, "td");
-        populateCell(bookRow, book.read, "td");
-        table.appendChild(bookRow);
-    }
+    let bookRow = document.createElement("tr");
+    populateCell(bookRow, book.title, "td");
+    populateCell(bookRow, book.author, "td");
+    populateCell(bookRow, book.pages, "td");
+    populateCell(bookRow, book.read, "td");
+    table.appendChild(bookRow);
 }
 
 function populateCell(row, text, type) {
@@ -48,4 +54,5 @@ function populateCell(row, text, type) {
     cell.textContent = text;
     row.appendChild(cell);
 }
+
 
